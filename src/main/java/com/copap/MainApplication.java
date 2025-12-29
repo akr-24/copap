488994214a830d5708a294ad1e7e70938101df59
Aman <amankumarg014@gmail.com>
2025-12-29 16:35:57 +0530

    adding order service and testing concurrency

--- src/main/java/com/copap/MainApplication.java
+++ src/main/java/com/copap/MainApplication.java
@@ -1,8 +1,13 @@
 package com.copap;
+
 import com.copap.model.*;
 import com.copap.repository.*;
+import com.copap.service.*;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainApplication {
     public static void main(String[] args) {
 
@@ -11,19 +16,36 @@ public class MainApplication {
 
         Product product1 = new Product("P1", "phone", 12);
         Product product2 = new Product("P2", "charger", 6);
+        Product p = new Product("P", "adapter", 9);
 
-        Order order = new Order("O1", customer, List.of(product1, product2));
-
-        System.out.println("Order ID: " + order.getOrderId());
-        System.out.println("Status: " + order.getStatus());
-        System.out.println("Total: " + order.totalAmount());
+//        Order order = new Order("O1", customer, List.of(product1, product2));
+//
+//        System.out.println("Order ID: " + order.getOrderId());
+//        System.out.println("Status: " + order.getStatus());
+//        System.out.println("Total: " + order.totalAmount());
 
         OrderRepository repository = new InMemoryOrderRepository();
+        OrderService service = new OrderService(repository);
+//        repository.save(order);
+//
+//        repository.findById("O1").ifPresent(o ->
+//                System.out.println("Found order with status: " + o.getStatus())
+//        );
 
-        repository.save(order);
+        ExecutorService executor = Executors.newFixedThreadPool(5);
 
-        repository.findById("O1").ifPresent(o ->
-                System.out.println("Found order with status: " + o.getStatus())
-        );
+        for (int i = 0; i < 10; i++) {
+            executor.submit(() -> {
+                Order order = new Order("O1", customer, List.of(p));
+                try {
+                    service.createOrder(order);
+                    System.out.println("Order created by " + Thread.currentThread().getName());
+                } catch (Exception e) {
+                    System.out.println("Failed by " + Thread.currentThread().getName());
+                }
+            });
+        }
+
+        executor.shutdown();
     }
 }
---
+++ src/main/java/com/copap/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.copap.service;
+
+import com.copap.repository.OrderRepository;
+import com.copap.model.Order;
+
+public class OrderService {
+
+    private final OrderRepository repository;
+
+    public OrderService(OrderRepository repository) {
+        this.repository = repository;
+    }
+
+    public void createOrder(Order order) {
+        /**
+         * this will not handle concurrency as Multiple threads
+         * can create the order because the existence check and save are not atomic.
+         *
+         */
+        if (repository.exists(order.getOrderId())) {
+            throw new IllegalStateException("Order already exists");
+        }
+        repository.save(order);
+    }
+}
